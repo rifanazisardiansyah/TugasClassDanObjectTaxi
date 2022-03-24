36a60acf02fa22be8505c577b10242d9f2fafb66
ASUS <ASUS@RIFANAZIS>
2022-03-24 16:26:43 +0700

    Add project files.

---
+++ TugasClassDanObjectTaxi/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasClassDanObjectTaxi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //membuat objek Taxi
+            Taxi taxi = new Taxi();
+
+            //pengesetan nilai properties
+            taxi.DriverName = "Jono ";
+            taxi.OnDuty = " True ";
+            taxi.NumPassenger = 10;
+
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
